572ae5342d3cec45bd8a3a701c0786c0b60791a5
Danny Beckett <k1w1onoe@hotmail.com>
2018-06-13 11:55:36 +1200

    Code Changes - Added Database Class and constructors to application

---
+++ WishList/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WishList.Data
+{
+
+    // Code Changes - Added Database Class and constructors to application
+
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
