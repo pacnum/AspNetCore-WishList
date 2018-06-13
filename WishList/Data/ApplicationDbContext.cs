b11a4dd2cbd33577c643d2fdf93595d42195a4c5
Danny Beckett <k1w1onoe@hotmail.com>
2018-06-13 12:13:47 +1200

    Another commit message

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+// Code Changes - Added Database Class and constructors to application
+//New class -  ApplicationDbContext created
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+    }
+}
